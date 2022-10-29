a9fd26de01e8b06bd94d9adf8e4ffcdf71f56d1f
Ruben Allenstein <ru.allenstein@reply.de>
2022-10-29 21:43:58 +0200

    [ADD] adding new controller to show all member

---
+++ src/Controller/ShowMemberController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShowMemberController extends AbstractController
+{
+    #[Route('/showMember', name: 'app_show_member')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'title' => 'avengers',
+        ]);
+
+    }
+}
