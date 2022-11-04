40b02d3bdf0e9d07ae949e24a0302c6a79bd8aae
Ruben Allenstein <ru.allenstein@reply.de>
2022-11-04 10:43:33 +0100

    [UPDATE] add table and output user database table

--- src/Controller/ShowMemberController.php
+++ src/Controller/ShowMemberController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShowMemberController extends AbstractController
 {
-    #[Route('/showMember', name: 'app_show_member')]
+    private UserRepository $userRepository;
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    #[Route('/memberList', name: 'app_show_member')]
     public function index(): Response
     {
+        $user = $this->userRepository->findAll();
         return $this->render('showMember/index.html.twig', [
-            'title' => 'avengers',
+            'user' => $user,
         ]);
-
     }
 }
