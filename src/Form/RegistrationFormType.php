c33b32b58631e7f10f928919ed5049887af5a6a8
Ruben Allenstein <ru.allenstein@reply.de>
2022-11-08 20:24:35 +0100

    [UPDATE] adding label and attributes for properties

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,8 +18,22 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstName')
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'autocomplete' => 'email',
+                    'class' => 'mx-5 px-4 my-3 btn btn-light',
+                    'placeholder' => 'Email'
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Vorname',
+                'attr' => [
+                    'autocomplete' => 'firstName',
+                    'class' => 'mx-5 px-4 my-3 btn btn-light',
+                    'placeholder' => 'Vorname'
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -30,8 +45,14 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => false,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'mx-5 px-3 my-3 btn btn-warning px-2',
+                    'placeholder' => 'Password'
+
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
